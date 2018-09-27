0e569534b76687ac9a506de9d826c83d94b61279
Bovin <bovindesouza@gmail.com>
2018-09-27 20:09:53 +0100

    updated php contact form validation for invalid characteristics for username, no major changes with index.css

--- php/contactform.php
+++ php/contactform.php
@@ -14,12 +14,21 @@ if (isset($_POST['submit'])) {
     $txt = "received e-mail from: ".$name." and message is: ".$message;
  
     
-    
+    //checks if input fields are left empty, throws out header message 
     if(empty($name) || empty($email) || empty($message)) {
         
-        header("location: http://bovin-desouza.co.uk/index.html?message=empty");
+        header("location: http://bovin-desouza.co.uk/index.html?message=inputsempty");
         
     } else {
+        //check if input characters are valid
+        //---test---//
+        if(!preg_match("/^[a-zA-Z]*$/", $name)) {
+            header("location: http://bovin-desouza.co.uk/index.html?message=invalidcharacters");
+            exit();
+        } else {
+            
+        
+        //checks if email is valid if not sends out a header message 
         if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             header("location: http://bovin-desouza.co.uk/index.html?message=invalidemail");
         } else {
@@ -27,8 +36,8 @@ if (isset($_POST['submit'])) {
             header("location: http://bovin-desouza.co.uk/index.html?message=success");
      
             mail($mailTo, $txt, $headers);
- 
         }
+      }
     }
     
 } else {
