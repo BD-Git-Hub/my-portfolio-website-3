bfef4561a6f18098131913ea53608de9450c6606
Bovin <bovindesouza@gmail.com>
2018-11-17 09:34:06 +0000

    php added exit function to if empty name, email,message condition

--- php/contactform.php
+++ php/contactform.php
@@ -12,28 +12,28 @@ if (isset($_POST['submit'])) {
     $mailTo = "bov@bovin-desouza.co.uk";
     $headers = "From: ".$mailFrom;
     $txt = "received e-mail from: ".$name." and message is: ".$message;
- 
-    
+   
     //checks if input fields are left empty, throws out header message 
     if(empty($name) || empty($email) || empty($message)) {
         
-        header("location: http://bovin-desouza.co.uk/index.html?message=inputsempty");
+        header("location: http://bovin-desouza.co.uk/index.html?message=inputEmpty");
+        exit();
         
     } else {
         //check if input characters are valid
         //---test---//
         if(!preg_match("/^[a-zA-Z]*$/", $name)) {
-            header("location: http://bovin-desouza.co.uk/index.html?message=invalidcharacters");
+            header("location: http://bovin-desouza.co.uk/index.html?message=invalidCharacters");
             exit();
         } else {
             
         
         //checks if email is valid if not sends out a header message 
         if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            header("location: http://bovin-desouza.co.uk/index.html?message=invalidemail");
+            header("location: http://bovin-desouza.co.uk/index.html?message=invalidEmail");
         } else {
             
-            header("location: http://bovin-desouza.co.uk/index.html?message=success");
+            header("location: http://bovin-desouza.co.uk/index.html?message=Success");
      
             mail($mailTo, $txt, $headers);
         }
